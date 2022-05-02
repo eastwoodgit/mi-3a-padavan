3675f1529b3dd67848bf45594d03a8305c474d0a
Xeath <xiaopiao.co.cc@gmail.com>
2022-05-02 21:04:56 +0800

    添加 JCG Q10 Pro

---
+++ trunk/configs/boards/JCG-Q10_Pro/board.h
@@ -0,0 +1,31 @@
+
+#define BOARD_PID		"JCG-Q10-Pro"
+#define BOARD_NAME		"JCG-Q10-Pro"
+#define BOARD_DESC		"JCG-Q10-Pro Wireless Router"
+#define BOARD_VENDOR_NAME	"JCG"
+#define BOARD_VENDOR_URL	"http://www.jcgcn.com/"
+#define BOARD_MODEL_URL		"http://www.jcgcn.com/"
+#define BOARD_BOOT_TIME		20
+#define BOARD_FLASH_TIME	120
+#define BOARD_GPIO_BTN_RESET	18
+#define BOARD_GPIO_BTN_WPS	16
+#undef  BOARD_GPIO_LED_ALL
+#undef  BOARD_GPIO_LED_WIFI
+#define BOARD_GPIO_LED_WAN	15
+#define BOARD_GPIO_LED_POWER	14	/* sys_blue: 15, sys_red: 14 */
+#undef  BOARD_GPIO_LED_LAN
+#undef  BOARD_GPIO_LED_USB
+#undef  BOARD_GPIO_LED_ROUTER
+#undef  BOARD_GPIO_PWR_USB_ON
+#undef  BOARD_GPIO_PWR_USB
+#define BOARD_HAS_5G_11AC	1
+#define BOARD_HAS_5G_11AX	1
+#define BOARD_HAS_2G_11AX	1
+#define BOARD_NUM_ANT_5G_TX	2
+#define BOARD_NUM_ANT_5G_RX	2
+#define BOARD_NUM_ANT_2G_TX	2
+#define BOARD_NUM_ANT_2G_RX	2
+#define BOARD_NUM_ETH_LEDS	0
+#define BOARD_NUM_ETH_EPHY	3
+#define BOARD_HAS_EPHY_L1000	1
+#define BOARD_HAS_EPHY_W1000	1
